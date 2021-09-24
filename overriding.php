d2a7b6ece45f9c34b9dea1e363a379c7aff1b3aa
Dival RF <divalrmd12@gmail.com>
2021-09-24 20:32:04 +0700

    Menyelesaikan video latihan mengenai overriding

--- overriding.php
+++ overriding.php
@@ -71,7 +71,7 @@ class CetakInfoProduk{
 }
 
 
-$produk1 = new Komik("Naruto","Masashi Kishimoto", "Shonen Jump", 30000, 100 );
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100 );
 $produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
 
 
